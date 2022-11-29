045f6fbf036feda5e956184147ffba3f9fe04d47
Kumar Kaustubh <119106399+kKaustubh25@users.noreply.github.com>
2022-11-29 15:13:39 +0530

    Update items.py

--- items.py
+++ items.py
@@ -12,36 +12,36 @@ Items = {
       "Dal" : 8.5
 }
 
-ItemList = dict()
-for Item in Items:
+itemlist = dict()
+for item in Items:
     print (Items)
 
 hasItem=True
 while (hasItem):
     print("Enter the items name you want to buy:")
-    Item = input ()
-    Item_price = input ("Enter the item price :")
+    item = input ()
+    item_price = input ("Enter the item price :")
     #convert to number
     item_price=int(item_price)
-    hasItem = input("Want to buy another (yes):")
+    hasitem = input("Want to buy another (yes):")
                   
-    if( hasItem == "yes"):
-       hasItem = True
+    if( hasitem == "yes"):
+       hasitem = True
     else:
-         hasItem = False
-    ItemList[items] = Items_Price
+         hasitem = False
+    itemlist[Items] = item_price
 
 m=" " *3 + "Name" + " " *3 + "+" + " " *3 +"Cost" + " " *3 + " " *3 +"+" "GST" +" " *3
 print (m)
 print ("-"*len(m))
 total =0
-for item in Itemlist:
+for item in itemlist:
     gst=items.get(item)
 #price = cost (1+gst)
 #cost = price / (1+gst)
 
     gst=gst/100
-    cost = itemList[item] / (1+gst)
+    cost = itemlist[item] / (1+gst)
     print (item +" " * 3 + "+" + " " *3 +
 str(int(cost) )+ " " *3 + "+" + " " *3 +
 str (float(gst*100) )+"%")
@@ -49,5 +49,5 @@ price=cost+(1+gst)
 total = total + price
 
 print("-"*len(m))
-print( "Total" + " " * 3 "+" + " " * 9 + str(int(total)) + "rupees")
+print( "Total" + " " * 3 +  "+" + " " * 9 + str(int(total)) + "rupees")
       
